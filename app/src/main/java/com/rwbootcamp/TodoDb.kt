6af285f3d729e4e52e6a228292c9390c70073985
Hilal <hilaltokgoz98@gmail.com>
2022-06-02 11:57:52 +0300

    Initial commit

---
+++ app/src/main/java/com/rwbootcamp/TodoDb.kt
@@ -0,0 +1,13 @@
+package com.rwbootcamp
+
+// Code with ❤️
+//┌──────────────────────────┐
+//│ Created by Hilal TOKGOZ  │
+//│ ──────────────────────── │
+//│ hilaltokgoz98@gmail.com  │
+//│ ──────────────────────── │
+//│ 2.06.2022                │
+//└──────────────────────────┘
+
+object TodoDb {
+}
